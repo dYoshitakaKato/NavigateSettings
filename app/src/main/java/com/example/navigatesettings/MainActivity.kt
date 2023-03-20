89fb0f0a916555c53a9822d5549ae53d5215b892
YoshitakaKato <yoshitaka.kato@l-is-b.com>
2023-03-20 10:15:44 +0900

    PreView用のメソッド作成

--- app/src/main/java/com/example/navigatesettings/MainActivity.kt
+++ app/src/main/java/com/example/navigatesettings/MainActivity.kt
@@ -1,15 +1,20 @@
 package com.example.navigatesettings
 
+import android.content.res.Configuration.UI_MODE_NIGHT_NO
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.navigatesettings.data.Setting
 import com.example.navigatesettings.ui.theme.NavigateSettingsTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,27 +22,62 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             NavigateSettingsTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+                MyApp(modifier = Modifier.fillMaxSize(), listOf())
+           }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MyApp(modifier: Modifier = Modifier, settings: List<Setting>) {
+    // A surface container using the 'background' color from the theme
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    ) {
+        SettingList(settings = settings)
+    }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true,
+    widthDp = 320,
+    heightDp = 320,
+    uiMode = UI_MODE_NIGHT_NO,
+    name = "PreviewLight")
 @Composable
-fun DefaultPreview() {
+fun LightPreviewApp() {
     NavigateSettingsTheme {
-        Greeting("Android")
+        PreviewApp()
     }
+}
+
+@Preview(showBackground = true,
+    widthDp = 320,
+    heightDp = 320,
+    uiMode = UI_MODE_NIGHT_YES,
+    name = "PreviewDark")
+@Composable
+fun DarkPreviewApp() {
+    NavigateSettingsTheme {
+        PreviewApp()
+    }
+}
+
+@Composable
+fun PreviewApp() {
+    MyApp(settings = listOf(Setting(name = "aiueo"), Setting("yoshitaka")))
+}
+
+@Composable
+    fun SettingList(settings: List<Setting>) {
+    LazyColumn {
+        items(settings) {
+            Setting(it)
+        }
+    }
+}
+
+@Composable
+fun Setting(setting: Setting) {
+    Text(text = setting.name)
 }
